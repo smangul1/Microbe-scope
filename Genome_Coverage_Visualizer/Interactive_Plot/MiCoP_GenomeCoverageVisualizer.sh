a76dbaa22af965f57e09e14dd84a00605dfebc51
mealser <mealser@gmail.com>
2017-09-13 00:23:24 +0300

    New_Visulaizer

--- Genome_Coverage_Visualizer/Interactive_Plot/MiCoP_GenomeCoverageVisualizer.sh
+++ Genome_Coverage_Visualizer/Interactive_Plot/MiCoP_GenomeCoverageVisualizer.sh
@@ -4,15 +4,20 @@ set -e  # exit immediately if error occurs
 # module load python/3.6.1
 # module load bwa
 # module load samtools
-# sed -i -e 's/\r$//' MiCoP_GenomeCoverageVisualizer.sh
-# chmod +x MiCoP_GenomeCoverageVisualizer.sh
-# time ./MiCoP_GenomeCoverageVisualizer.sh /u/project/zarlab/serghei/eupathdb/eupathdbFasta/piroplasma.fa /u/home/galaxy/collaboratory/serghei/MetaSUB-Inter-City-Challenge/data/SRR3546361.fastq /u/project/zarlab/malser/MiCoP/Scripts /u/project/zarlab/malser/MiCoP 200 1 2 /u/scratch2/scratch2/m/malser/HomologyInformation/ 30
+# sed -i -e 's/\r$//' /u/project/zarlab/malser/MiCoP/Scripts/MiCoP_GenomeCoverageVisualizer.sh
+# chmod +x /u/project/zarlab/malser/MiCoP/Scripts/MiCoP_GenomeCoverageVisualizer.sh
+# /u/project/zarlab/malser/MiCoP/Scripts/MiCoP_GenomeCoverageVisualizer.sh /u/scratch2/scratch2/m/malser/MergedEuPathDB /u/home/galaxy/collaboratory/serghei/MetaSUB-Inter-City-Challenge/data/SRR3546361.fastq /u/project/zarlab/malser/MiCoP/Scripts /u/project/zarlab/malser/MiCoP 1 /u/scratch2/scratch2/m/malser/HomologyInformation/ 30
 #########################################################################
 # Reference database (metagenome)
-metagenome=$1 									# /u/project/zarlab/serghei/eupathdb/eupathdbFasta/piroplasma.fa
-metagenomeBasename=`basename $metagenome`		# piroplasma.fa`
-metagenomeFName="${metagenomeBasename%.*}"		# piroplasma
-metagenomeExtention="${metagenomeBasename##*.}"	# fa
+metagenome=$1 									# /u/scratch2/scratch2/m/malser/MergedEuPathDB
+case "$metagenome" in
+*/)
+    metagenome="$metagenome"
+    ;;
+*)
+    metagenome="$metagenome/"
+    ;;
+esac
 #########################################################################
 # Microbiome under study
 sample=$2								# /u/home/galaxy/collaboratory/serghei/MetaSUB-Inter-City-Challenge/data/SRR3546361.fastq
@@ -22,98 +27,88 @@ sampleExtention="${sampleBasename##*.}"	# fastq
 #########################################################################
 # Script Directory
 scriptDir=$3							# /u/project/zarlab/malser/MiCoP/Scripts
+case "$scriptDir" in
+*/)
+    scriptDir="$scriptDir"
+    ;;
+*)
+    scriptDir="$scriptDir/"
+    ;;
+esac
 #########################################################################
 # Create auxiliary folders
 dataDir=$4
-dataDir="$dataDir/$metagenomeFName/$sampleFName/"
+case "$dataDir" in
+*/)
+    dataDir="$dataDir"
+    ;;
+*)
+    dataDir="$dataDir/"
+    ;;
+esac
+dataDir="${dataDir}MiCoP_$sampleFName/"
 mkdir -p ${dataDir}
 #########################################################################
 # Coverage plot variables 
 windowSize=$5
 #########################################################################
 # Homology (RefSeq <-> EuPathDB) Information
-HomologyDir=$8                               # /u/scratch2/scratch2/m/malser/HomologyInformation/
-KmerSize=$9                                  # 30
+HomologyDir=$6                               # /u/scratch2/scratch2/m/malser/HomologyInformation/
+case "$HomologyDir" in
+*/)
+    HomologyDir="$HomologyDir"
+    ;;
+*)
+    HomologyDir="$HomologyDir/"
+    ;;
+esac
 #########################################################################
-# Call Necessary Tools
-if [ "$6" = "1" ]
-then
-	echo "UCLA Hoffman2 mode is selected!"
-	. /u/local/Modules/default/init/modules.sh
-	module load python/3.6.1
-	module load bwa
-	module load samtools
-elif [ "$6" = "2" ]
-then
-	echo "Non-Hoffman2 mode is selected"
-else
-	echo "Please select 1 or 2 for the sixth parameter!"
-	exit
-fi
+KmerSize=$7                                  # 30
 #########################################################################
-# Start Bulding the input data for the Genome Covergae Visualizer
-if [ "$7" = "1" ]
-then
-	# Concatenate all contigs of the reference database
-	echo "First step: Contigs Concatenation"
-	grep '>' ${metagenome} | awk -F "|" '{print $2}' | uniq > ${dataDir}${metagenomeFName}_RefList_perGenome.txt
-	python3 ${scriptDir}/ConcatContigs.py ${dataDir}${metagenomeFName}_RefList_perGenome.txt ${metagenome} > ${dataDir}${metagenomeFName}_ConcatContigs.${metagenomeExtention}
 
-	# BWA-MEM Mapping
-	echo "Next step: BWA-MEM Indexing, Mapping, & Sorting"
-	bwa index ${dataDir}${metagenomeFName}_ConcatContigs.${metagenomeExtention}
-	bwa mem -a -v 0 ${dataDir}${metagenomeFName}_ConcatContigs.${metagenomeExtention} ${sample} | awk '$3!="*"' | samtools view -bS - > ${dataDir}${metagenomeFName}_${sampleFName}_Filtered.bam
-	samtools view -H ${dataDir}${metagenomeFName}_${sampleFName}_Filtered.bam > ${dataDir}header.txt  
-	samtools view ${dataDir}${metagenomeFName}_${sampleFName}_Filtered.bam | sort -t$'\t' -V -k 1,1n -V -k 3,3  | cat ${dataDir}header.txt -| samtools view -bS - >  ${dataDir}${metagenomeFName}_${sampleFName}_Sorted.bam
+#########################################################################
+#########################################################################
+#########################################################################
+# BWA-MEM Mapping
+echo "First step: BWA-MEM Mapping, & Sorting"
+bwa mem -a -v 0 ${metagenome}EuPathDB_Merged_amoebadb_ConcatContigs.fasta ${sample} | awk '(!/^ *@/) && ($3!="*")' >> ${dataDir}${sampleFName}_BWA-MEM.sam
+bwa mem -a -v 0 ${metagenome}EuPathDB_Merged_cryptodb_ConcatContigs.fasta ${sample} | awk '(!/^ *@/) && ($3!="*")' >> ${dataDir}${sampleFName}_BWA-MEM.sam
+bwa mem -a -v 0 ${metagenome}EuPathDB_Merged_fungidb_ConcatContigs.fasta ${sample} | awk '(!/^ *@/) && ($3!="*")'  >> ${dataDir}${sampleFName}_BWA-MEM.sam
+bwa mem -a -v 0 ${metagenome}EuPathDB_Merged_giardiadb_ConcatContigs.fasta ${sample} | awk '(!/^ *@/) && ($3!="*")'  >> ${dataDir}${sampleFName}_BWA-MEM.sam
+bwa mem -a -v 0 ${metagenome}EuPathDB_Merged_microsporidiadb_ConcatContigs.fasta ${sample} | awk '(!/^ *@/) && ($3!="*")' >> ${dataDir}${sampleFName}_BWA-MEM.sam
+bwa mem -a -v 0 ${metagenome}EuPathDB_Merged_NCBI-RefSeq-viral_ConcatContigs.fasta ${sample} | awk '(!/^ *@/) && ($3!="*")' >> ${dataDir}${sampleFName}_BWA-MEM.sam
+bwa mem -a -v 0 ${metagenome}EuPathDB_Merged_piroplasmadb_ConcatContigs.fasta ${sample} | awk '(!/^ *@/) && ($3!="*")' >> ${dataDir}${sampleFName}_BWA-MEM.sam
+bwa mem -a -v 0 ${metagenome}EuPathDB_Merged_plasmodb_ConcatContigs.fasta ${sample} | awk '(!/^ *@/) && ($3!="*")' >> ${dataDir}${sampleFName}_BWA-MEM.sam
+bwa mem -a -v 0 ${metagenome}EuPathDB_Merged_toxodb_ConcatContigs.fasta ${sample} | awk '(!/^ *@/) && ($3!="*")' >> ${dataDir}${sampleFName}_BWA-MEM.sam
+bwa mem -a -v 0 ${metagenome}EuPathDB_Merged_trichdb_ConcatContigs.fasta ${sample} | awk '(!/^ *@/) && ($3!="*")' >> ${dataDir}${sampleFName}_BWA-MEM.sam
+bwa mem -a -v 0 ${metagenome}EuPathDB_Merged_tritrypdb_ConcatContigs.fasta ${sample} | awk '(!/^ *@/) && ($3!="*")' >> ${dataDir}${sampleFName}_BWA-MEM.sam
 
-	# Generate Reference List to be used for sorting the reads
-	grep '>' ${dataDir}${metagenomeFName}_ConcatContigs.${metagenomeExtention} > ${dataDir}${metagenomeFName}_RefList_perGenome.txt
-elif [ "$7" = "2" ]
-then
-	# BWA-MEM Mapping
-	echo "First step: BWA-MEM Indexing, Mapping, & Sorting"
-	cp  ${metagenome} ${dataDir}
-	bwa index ${dataDir}${metagenomeBasename}
-	bwa mem -a -v 0 ${dataDir}${metagenomeBasename} ${sample} | awk '$3!="*"' | samtools view -bS - > ${dataDir}${metagenomeFName}_${sampleFName}_Filtered.bam
-	samtools view -H ${dataDir}${metagenomeFName}_${sampleFName}_Filtered.bam > ${dataDir}header.txt  
-	samtools view ${dataDir}${metagenomeFName}_${sampleFName}_Filtered.bam | sort -t$'\t' -V -k 1,1n -V -k 3,3  | cat ${dataDir}header.txt -| samtools view -bS - >  ${dataDir}${metagenomeFName}_${sampleFName}_Sorted.bam
-
-	# Generate Reference List to be used for sorting the reads
-	grep '>' ${dataDir}${metagenomeBasename} | awk -F "|" '{print $2}' | uniq > ${dataDir}${metagenomeFName}_RefList_perContigs.txt
-	python3 ${scriptDir}/CalculateGenomeLengthFromContigs.py ${dataDir}${metagenomeFName}_RefList_perContigs.txt ${metagenome} > ${dataDir}${metagenomeFName}_RefList_perGenome.txt
-elif [ "$7" = "3" ]
-then
-	# Sorting the input .bam file
-	echo "First step: Sorting BWA-MEM's output"
-	samtools view -H $10 > ${dataDir}header.txt  
-	samtools view $10 | sort -t$'\t' -V -k 1,1n -V -k 3,3 | cat ${dataDir}header.txt -| samtools view -bS - >  ${dataDir}${metagenomeFName}_${sampleFName}_Sorted.bam
-
-	# Generate Reference List to be used for sorting the reads
-	grep '>' ${dataDir}${metagenomeFName}_ConcatContigs.${metagenomeExtention} > ${dataDir}${metagenomeFName}_RefList_perGenome.txt
-else
-	echo "Please select 1 or 2 for the seventh parameter! (1) for concatenating the contigs of the reference database. (2) for not concatenating the contigs."
-	exit
-fi
+# Sort .bam content
+sort -t$'\t' -V -k 1,1n -V -k 3,3 ${dataDir}${sampleFName}_BWA-MEM.sam > ${dataDir}${sampleFName}_BWA-MEM_sorted.sam
 #########################################################################
 # Unique Read
-echo "Next step: Unique Read Coverage"
-#samtools view ${dataDir}${metagenomeFName}_${sampleFName}_Sorted.bam | python3 ${scriptDir}/ReadClassifier_and_Filter.py /dev/fd/0  1 | sort -t$'\t' -V -k 3,3 > ${dataDir}${metagenomeFName}_${sampleFName}_UniqueReads_Sorted.sam
-samtools view ${dataDir}${metagenomeFName}_${sampleFName}_Sorted.bam | awk 'BEGIN { FS="\t" } { c[$1]++; l[$1,c[$1]]=$0 } END { for (i in c) { if (c[i] == 1) for (j = 1; j <= c[i]; j++) print l[i,j] } }' | sort -t$'\t' -k 3,3 > ${dataDir}${metagenomeFName}_${sampleFName}_UniqueReads_Sorted.sam
-python3 ${scriptDir}/CoveragePlot_HighChartsSingleCSVperGenome.py ${dataDir}${metagenomeFName}_${sampleFName}_UniqueReads_Sorted.sam ${dataDir}${metagenomeFName}_RefList_perGenome.txt ${windowSize} 1 ${dataDir}
+echo "Second step: Unique Read Coverage"
+#samtools view ${dataDir}${sampleFName}_Sorted.bam| python3 ${scriptDir}/ReadClassifier_and_Filter.py /dev/fd/0  1 | sort -t$'\t' -V -k 3,3 > ${dataDir}${sampleFName}_UniqueReads_Sorted.sam
+awk 'BEGIN { FS="\t" } { c[$1]++; l[$1,c[$1]]=$0 } END { for (i in c) { if (c[i] == 1) for (j = 1; j <= c[i]; j++) print l[i,j] } }' ${dataDir}${sampleFName}_BWA-MEM_sorted.sam| sort -t$'\t' -k 3,3 > ${dataDir}${sampleFName}_UniqueReads_Sorted.sam
+python3 ${scriptDir}/CoveragePlot_HighChartsSingleCSVperGenome.py ${dataDir}${sampleFName}_UniqueReads_Sorted.sam ${metagenome}MiCoP_DB_RefList_perGenome.txt ${windowSize} 1 ${dataDir}
 #########################################################################
 # Multimapped Read (within genome)
-echo "Next step: Multimapped Read (within genome) Coverage"
-samtools view ${dataDir}${metagenomeFName}_${sampleFName}_Sorted.bam | python3 ${scriptDir}/ReadClassifier_and_Filter.py /dev/fd/0  2 | sort -t$'\t' -V -k 3,3 > ${dataDir}${metagenomeFName}_${sampleFName}_MMWithinReads_Sorted.sam
-python3 ${scriptDir}/CoveragePlot_HighChartsSingleCSVperGenome.py ${dataDir}${metagenomeFName}_${sampleFName}_MMWithinReads_Sorted.sam ${dataDir}${metagenomeFName}_RefList_perGenome.txt ${windowSize} 2 ${dataDir}
+echo "Third step: Multimapped Read (within genome) Coverage"
+python3 ${scriptDir}/ReadClassifier_and_Filter.py ${dataDir}${sampleFName}_BWA-MEM_sorted.sam 2 | sort -t$'\t' -V -k 3,3 > ${dataDir}${sampleFName}_MMWithinReads_Sorted.sam
+python3 ${scriptDir}/CoveragePlot_HighChartsSingleCSVperGenome.py ${dataDir}${sampleFName}_MMWithinReads_Sorted.sam ${metagenome}MiCoP_DB_RefList_perGenome.txt ${windowSize} 2 ${dataDir}
 #########################################################################
 # Multimapped Read (across genome)
-echo "Next step: Multimapped Read (across genome) Coverage"
-samtools view ${dataDir}${metagenomeFName}_${sampleFName}_Sorted.bam | python3 ${scriptDir}/ReadClassifier_and_Filter.py /dev/fd/0  3 | sort -t$'\t' -V -k 3,3 > ${dataDir}${metagenomeFName}_${sampleFName}_MMAcrossReads_Sorted.sam
-python3 ${scriptDir}/CoveragePlot_HighChartsSingleCSVperGenome.py ${dataDir}${metagenomeFName}_${sampleFName}_MMAcrossReads_Sorted.sam ${dataDir}${metagenomeFName}_RefList_perGenome.txt ${windowSize} 3 ${dataDir}
+echo "Four step: Multimapped Read (across genome) Coverage"
+python3 ${scriptDir}/ReadClassifier_and_Filter.py ${dataDir}${sampleFName}_BWA-MEM_sorted.sam 3 | sort -t$'\t' -V -k 3,3 > ${dataDir}${sampleFName}_MMAcrossReads_Sorted.sam
+python3 ${scriptDir}/CoveragePlot_HighChartsSingleCSVperGenome.py ${dataDir}${sampleFName}_MMAcrossReads_Sorted.sam ${metagenome}MiCoP_DB_RefList_perGenome.txt ${windowSize} 3 ${dataDir}
 #########################################################################
-# Finalizing the output .csv files
-echo "Last step: Finalizing the output .csv files"
-python3 ${scriptDir}/FilterNulls_csv.py ${dataDir}MiCoP_FinalCSVs ${dataDir}${metagenomeFName}_RefList_perGenome.txt ${HomologyDir} ${KmerSize} ${windowSize}
+# Homologous Read Coverage and Finalizing the output .csv files
+echo "Fifth step: Homologous Read Coverage and finalizing the output .csv files"
+python3 ${scriptDir}/FilterNulls_csv.py ${dataDir}MiCoP_FinalCSVs ${metagenome}MiCoP_DB_RefList_perGenome.txt ${HomologyDir} ${KmerSize} ${windowSize}
+#########################################################################
+# Deleting Temporary Files
+echo "Last step: Deleting Temporary Files"
+#rm -f ${dataDir}header.txt
+#find ${dataDir} -maxdepth 1 -name "${sampleFName}_*.bam" -type f -delete
 
- /u/scratch2/scratch2/m/malser/HomologyInformation/ 30 200
 echo "MiCoP Genome Coverage Visualizer is ready ... copy the following folder $dataDirMiCoP_FinalCSVs next to MiCoP.html from Scripts folder and open MiCoP.html"
